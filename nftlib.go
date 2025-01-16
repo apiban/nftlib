4609321e82c4dff883251a3d78586be065183c58
Fred Posner <fred@pgpx.io>
2025-01-16 11:05:02 -0500

    License/Functions
    * License -> MIT
    * various nft functions (get chain details, get tables, etc)

--- nftlib.go
+++ nftlib.go
@@ -33,6 +33,20 @@ type NFTABLES struct {
 	Elements []string `json:"elements"`
 }
 
+type NFTCHAINDETAILS struct {
+	Table  string `json:"table"`
+	Family string `json:"family"`
+	Chain  string `json:"chain"`
+	Hook   string `json:"hook"`
+	Type   string `json:"type"`
+}
+
+type NFTTABLEDETAILS struct {
+	Table  string `json:"table"`
+	Family string `json:"family"`
+	Handle int    `json:"handle"`
+}
+
 // nft list add element to set
 func NftAddSetElement(data NFTABLES, ipaddress string) error {
 	args := []string{"add", "element", data.Family, data.Table, data.Set, "{", ipaddress, "}"}
@@ -66,6 +80,166 @@ func NftFlushSet(data NFTABLES) error {
 	return nil
 }
 
+// list filter type chains
+func NftGetChainDetails(chainname string) (NFTCHAINDETAILS, error) {
+	var chaindetails NFTCHAINDETAILS
+	args := []string{"-j", "list", "chains"}
+	response, err := exec.Command("nft", args...).Output()
+	if err != nil {
+		return chaindetails, err
+	}
+
+	if !gjson.Valid(string(response)) {
+		return chaindetails, errors.New("invalid json response")
+	}
+
+	chains := gjson.Get(string(response), "nftables.#(chain.name==\""+chainname+"\").chain")
+	if !chains.Exists() {
+		return chaindetails, errors.New("chain not found")
+	}
+
+	family := gjson.Get(string(response), "nftables.#(chain.name==\""+chainname+"\").chain.family")
+	if !family.Exists() {
+		return chaindetails, errors.New("cannot get chain family")
+	}
+
+	table := gjson.Get(string(response), "nftables.#(chain.name==\""+chainname+"\").chain.handle")
+	if !table.Exists() {
+		return chaindetails, errors.New("cannot get chain table")
+	}
+
+	hook := gjson.Get(string(response), "nftables.#(chain.name==\""+chainname+"\").chain.hook")
+	if !hook.Exists() {
+		return chaindetails, errors.New("cannot get chain hook")
+	}
+
+	chaintype := gjson.Get(string(response), "nftables.#(chain.name==\""+chainname+"\").chain.type")
+	if !chaintype.Exists() {
+		return chaindetails, errors.New("cannot get chain type")
+	}
+
+	chaindetails.Family = family.String()
+	chaindetails.Table = table.String()
+	chaindetails.Hook = hook.String()
+	chaindetails.Chain = chainname
+	chaindetails.Type = chaintype.String()
+
+	return chaindetails, nil
+}
+
+// list filter type chains
+func NftGetFilterChains() ([]string, error) {
+	var nftchains []string
+	args := []string{"-j", "list", "chains"}
+	response, err := exec.Command("nft", args...).Output()
+	if err != nil {
+		return nftchains, err
+	}
+
+	if !gjson.Valid(string(response)) {
+		return nftchains, errors.New("invalid json response")
+	}
+
+	chains := gjson.Get(string(response), "nftables.#(chain.type==\"filter\")#.chain.name")
+	if chains.Exists() {
+		chains.ForEach(func(key, value gjson.Result) bool {
+			nftchains = append(nftchains, value.String())
+			return true
+		})
+	} else {
+		return nftchains, errors.New("no filter chains found")
+	}
+
+	return nftchains, nil
+}
+
+// list input hook chains
+func NftGetInputChains() ([]string, error) {
+	var nftchains []string
+	args := []string{"-j", "list", "chains"}
+	response, err := exec.Command("nft", args...).Output()
+	if err != nil {
+		return nftchains, err
+	}
+
+	if !gjson.Valid(string(response)) {
+		return nftchains, errors.New("invalid json response")
+	}
+
+	chains := gjson.Get(string(response), "nftables.#(chain.hook==\"input\")#.chain.name")
+	if chains.Exists() {
+		chains.ForEach(func(key, value gjson.Result) bool {
+			nftchains = append(nftchains, value.String())
+			return true
+		})
+	} else {
+		return nftchains, errors.New("no input hook chains found")
+	}
+
+	return nftchains, nil
+}
+
+// list tables
+func NftGetTables() ([]string, error) {
+	var nfttables []string
+	args := []string{"-j", "list", "tables"}
+	response, err := exec.Command("nft", args...).Output()
+	if err != nil {
+		return nfttables, err
+	}
+
+	if !gjson.Valid(string(response)) {
+		return nfttables, errors.New("invalid json response")
+	}
+
+	tables := gjson.Get(string(response), "nftables.#.table.name")
+	if tables.Exists() {
+		tables.ForEach(func(key, value gjson.Result) bool {
+			nfttables = append(nfttables, value.String())
+			return true
+		})
+	} else {
+		return nfttables, errors.New("no tables found")
+	}
+
+	return nfttables, nil
+}
+
+// get table info for table
+func NftGetTableInfo(tablename string) (NFTTABLEDETAILS, error) {
+	var tabledetails NFTTABLEDETAILS
+	args := []string{"-j", "list", "tables"}
+	response, err := exec.Command("nft", args...).Output()
+	if err != nil {
+		return tabledetails, err
+	}
+
+	if !gjson.Valid(string(response)) {
+		return tabledetails, errors.New("invalid json response")
+	}
+
+	tables := gjson.Get(string(response), "nftables.#(table.name==\""+tablename+"\").table")
+	if !tables.Exists() {
+		return tabledetails, errors.New("table not found")
+	}
+
+	family := gjson.Get(string(response), "nftables.#(table.name==\""+tablename+"\").table.family")
+	if !family.Exists() {
+		return tabledetails, errors.New("cannot get table family")
+	}
+
+	handle := gjson.Get(string(response), "nftables.#(table.name==\""+tablename+"\").table.handle")
+	if !handle.Exists() {
+		return tabledetails, errors.New("cannot get table handle")
+	}
+
+	tabledetails.Family = family.String()
+	tabledetails.Table = tablename
+	tabledetails.Handle = int(handle.Int())
+
+	return tabledetails, nil
+}
+
 func NftListSet(set string) (NFTABLES, error) {
 	var nftableData NFTABLES
 	args := []string{"-j", "list", "sets"}
@@ -78,7 +252,6 @@ func NftListSet(set string) (NFTABLES, error) {
 		return nftableData, errors.New("invalid json response")
 	}
 
-	//nftables.#(set=="set").set.elem
 	version := gjson.Get(string(sets), "nftables.#(@flatten).metainfo.version")
 	if !version.Exists() {
 		return nftableData, errors.New("invalid json response")
