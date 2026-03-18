f4dcf49d88162cc62c65e445c6b65408d4a9ccf5
Fred Posner <fred@pgpx.io>
2026-03-18 15:39:56 -0400

    license / uptime

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # nftlib
 
 NFT tools via os/exec. Uses `json` output of nft cli.
+
+## License
+
+`GPLv3`
+
+Copyright: Fred Posner ([Palner](https://www.palner.com/))
--- nftlib.go
+++ nftlib.go
@@ -1,33 +1,18 @@
 /*
 NFTLIB - exec commands to work with NFTABLES
-The MIT License (MIT)
-
-Copyright (c) 2025 Fred Posner
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+License: GPLv3
+Copyright (c) 2025,2026 Fred Posner
 */
 
 package nftlib
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/tidwall/gjson"
 )
@@ -52,6 +37,27 @@ type NFTTABLEDETAILS struct {
 	Table  string `json:"table"`
 	Family string `json:"family"`
 	Handle int    `json:"handle"`
+}
+
+// get linux uptime
+func LinuxUptime() (int64, error) {
+	data, err := os.ReadFile("/proc/uptime")
+	if err != nil {
+		return 0, fmt.Errorf("failed to read /proc/uptime: %w", err)
+	}
+
+	fields := strings.Fields(string(data))
+	if len(fields) < 1 {
+		return 0, fmt.Errorf("invalid /proc/uptime format")
+	}
+
+	uptimeSeconds, err := strconv.ParseFloat(fields[0], 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse uptime seconds: %w", err)
+	}
+
+	// Convert to int64
+	return int64(uptimeSeconds), nil
 }
 
 // nft add set
